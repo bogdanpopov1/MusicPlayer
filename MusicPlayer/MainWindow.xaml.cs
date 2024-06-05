6b6fde346ec3901518ef78e1f68cce43ef860ec9
Богдан Попов <bogdan_popov17@mail.ru>
2024-06-05 11:02:28 +0300

    Открытие файлов и папок

--- MusicPlayer/MainWindow.xaml.cs
+++ MusicPlayer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -15,14 +18,74 @@ using System.Windows.Shapes;
 
 namespace MusicPlayer
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private MediaPlayer _player = new MediaPlayer();
+        private List<FileInfo> fileInfoFolder = new List<FileInfo>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _player = new MediaPlayer();
+            this.DataContext = this;
+            FilesDG.ItemsSource = fileInfoFolder;
         }
+
+        private void OpenFileMI_Click(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                DefaultExt = ".mp3",
+                Multiselect = true,
+            };
+
+            bool? fileDialogOk = fileDialog.ShowDialog();
+
+            if (fileDialogOk == true)
+            {
+                var name = fileDialog.FileName.Split('\\').Last();
+                var filename = fileDialog.FileName;
+                FileNameLb.Content = $"Воспроизводится файл {name}";
+                _player.Open(new Uri(filename));
+            }
+        }
+
+        private void OpenFolderMI_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fileDialog = new FolderBrowserDialog();
+
+            var fileDialogOk = fileDialog.ShowDialog();
+
+            var filename = fileDialog.SelectedPath;
+
+            DirectoryInfo dirInfo = new DirectoryInfo(filename);
+
+            FileInfo[] fileInfo = dirInfo.GetFiles("*.mp3");
+            foreach (FileInfo f in fileInfo)
+            {
+                fileInfoFolder.Add(f);
+            }
+
+            FilesDG.Items.Refresh();
+        }
+
+        private void PlayBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Play();
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Stop();
+        }
+
+        private void PauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Pause();
+        }
+
+
     }
 }
